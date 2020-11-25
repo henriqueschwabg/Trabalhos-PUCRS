8f9d099c5f22ee383d4f121569ba0475603c30d9
hschwabg <schwabhenrique@gmail.com>
2020-11-25 10:10:16 -0300

    up

--- Visualização de Dados/main.py
+++ Visualização de Dados/main.py
@@ -1,14 +1,17 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+
+sns.set_style("whitegrid")
+
+# 4
 
 racacor_dataset = 'datasets/df_homicidios_racacor.csv'
 sexo_dataset = 'datasets/df_homicidios_sexo.csv'
 idade_dataset = 'datasets/df_homicidios_idade.csv'
 escolaridade_dataset = 'datasets/df_homicidios_escolaridade.csv'
-causasviolentas_dataset = 'datasets/df_causas_violentas.csv'
 
 
 @st.cache
@@ -22,9 +25,8 @@ df_racacor = load_data(racacor_dataset)
 df_sexo = load_data(sexo_dataset)
 df_escolaridade = load_data(escolaridade_dataset)
 df_idade = load_data(idade_dataset)
-df_causasviolentas = load_data(causasviolentas_dataset)
 
-st.sidebar.markdown("Homicídios por ano e estado.")
+st.sidebar.markdown("**Números de homicídios por ano e estado**")
 
 st.sidebar.subheader("Ano")
 ano_filtrado = st.sidebar.slider("Escolha o ano desejado", 2008, 2018, 2017)
@@ -51,14 +53,17 @@ if filtro_selecionado == 'Raça/Cor':
 
     colunas_filtradas = [col for col in df if (col.startswith(str(ano_filtrado))) or (col in ['UF', 'lat', 'lon'])]
     dados_filtrados = df.loc[(df['UF'] == estado_filtrado), colunas_filtradas]
-
     colunas_filtradas = [col for col in df if col.startswith(str(ano_filtrado))]
-    df_new = pd.DataFrame(dados_filtrados[colunas_filtradas].values.T)
-    ax = df_new.plot.bar(color=[['blue', 'black', 'green', 'brown', 'maroon']])
-    ax.set_title(f'Homicídios por {filtro_selecionado} em {ano_filtrado} no {estado_filtrado}')
-    ax.set_xticklabels(('Branco', 'Preta', 'Amarela', 'Parda', 'Indígena'))
-    ax.set_ylabel('quantidade')
-    ax.get_legend().remove()
+
+    df_new = pd.DataFrame()
+    df_new['A'] = ['Branco', 'Preta', 'Amarela', 'Parda', 'Indígena']
+    df_new['B'] = list(dados_filtrados[colunas_filtradas].values.T)
+    df_new['B'] = df_new['B'].astype(int)
+
+    sns.catplot('A', 'B', data=df_new)
+    plt.title(f'Homicídios por {filtro_selecionado} em {ano_filtrado} no {estado_filtrado}')
+    plt.xlabel('')
+    plt.ylabel('contagem')
 
     st.pyplot()
 
@@ -69,12 +74,13 @@ elif filtro_selecionado == 'Sexo':
     dados_filtrados = df.loc[(df['UF'] == estado_filtrado), colunas_filtradas]
     colunas_filtradas = [col for col in df if col.startswith(str(ano_filtrado))]
 
-    fig, ax = plt.subplots()
-    labels='Homem', 'Mulher'
+    labels ='Homem', 'Mulher'
     explode = (0.1, 0)
-    ax.pie(dados_filtrados[colunas_filtradas].values.T, explode=explode, labels=labels, autopct="%1.1f%%")
-    ax.set_title(f'Homicídios por {filtro_selecionado} em {ano_filtrado} no {estado_filtrado}')
-    # shadow=True, startangle=90
+    df_new = pd.DataFrame(dados_filtrados[colunas_filtradas].values.T)
+    df_new.plot.pie(subplots=True, explode=explode, labels=labels, autopct="%1.1f%%")
+    plt.title(f'Homicídios por {filtro_selecionado} em {ano_filtrado} no {estado_filtrado}')
+    plt.ylabel('')
+
     st.pyplot()
 
 elif filtro_selecionado == 'Escolaridade':
@@ -84,12 +90,15 @@ elif filtro_selecionado == 'Escolaridade':
     dados_filtrados = df.loc[(df['UF'] == estado_filtrado), colunas_filtradas]
     colunas_filtradas = [col for col in df if col.startswith(str(ano_filtrado))]
 
-    df_new = pd.DataFrame(dados_filtrados[colunas_filtradas].values.T)
-    ax = df_new.plot.bar(color=[['blue', 'black', 'green', 'brown', 'maroon']])
-    ax.set_title(f'Homicídios por {filtro_selecionado} em {ano_filtrado} no {estado_filtrado}')
-    ax.set_xticklabels(('Nenhuma', 'De 1 a 3 anos', 'De 4 a 7 anos', 'De 8 a 11 anos', '12 anos e mais'))
-    ax.set_ylabel('quantidade')
-    ax.get_legend().remove()
+    df_new = pd.DataFrame()
+    df_new['A'] = ['Nenhuma', 'De 1 a 3 anos', 'De 4 a 7 anos', 'De 8 a 11 anos', '12 anos e mais']
+    df_new['B'] = list(dados_filtrados[colunas_filtradas].values.T)
+    df_new['B'] = df_new['B'].astype(int)
+
+    sns.barplot('A', 'B', data=df_new)
+    plt.title(f'Homicídios por {filtro_selecionado} em {ano_filtrado} no {estado_filtrado}')
+    plt.xlabel('')
+    plt.ylabel('contagem')
 
     st.pyplot()
 
@@ -100,29 +109,26 @@ elif filtro_selecionado == 'Faixa de Idade':
     dados_filtrados = df.loc[(df['UF'] == estado_filtrado), colunas_filtradas]
     colunas_filtradas = [col for col in df if col.startswith(str(ano_filtrado))]
 
-    df_new = pd.DataFrame(dados_filtrados[colunas_filtradas].values.T)
-    ax = df_new.plot.bar(color=[['blue', 'black', 'green', 'brown', 'maroon']])
-    ax.set_title(f'Homicídios por {filtro_selecionado} em {ano_filtrado} no {estado_filtrado}')
-    ax.set_xticklabels(('0 - 4', '5 - 9', '10 - 14', '15 - 19', '20 - 24', '25 - 29', '30 - 34', '35 - 39',
-                        '40 - 44', '45 - 49', '50 - 54', '55 - 59', '60 - 64', '65 - 69', '70 - 74', '75 - 79', '80+'))
-    ax.set_ylabel('quantidade')
-    ax.get_legend().remove()
+    df_new = pd.DataFrame()
+    df_new['A'] = ['0 - 4', '5 - 9', '10 - 14', '15 - 19', '20 - 24', '25 - 29', '30 - 34', '35 - 39',
+                        '40 - 44', '45 - 49', '50 - 54', '55 - 59', '60 - 64', '65 - 69', '70 - 74', '75 - 79', '80+']
+    df_new['B'] = list(dados_filtrados[colunas_filtradas].values.T)
+    df_new['B'] = df_new['B'].astype(int)
+
+    sns.barplot('A', 'B', data=df_new)
+    plt.title(f'Homicídios por {filtro_selecionado} em {ano_filtrado} no {estado_filtrado}')
+    plt.xlabel('')
+    plt.ylabel('contagem')
+    plt.xticks(rotation=30, fontsize=6)
 
     st.pyplot()
 
+# 5
 
-### MAPA? ###
-# st.subheader("Mapa do Estado")
-# st.map(dados_filtrados)
+causasviolentas_dataset = 'datasets/df_causas_violentas.csv'
+df_causasviolentas = load_data(causasviolentas_dataset)
 
-# tabela (provavelmente temporária)
-# st.sidebar.subheader("Tabela")
-# tabela = st.sidebar.empty()
-# if tabela.checkbox("Mostrar tabela de dados"):
-    # st.write(dados_filtrados)
-
-
-st.sidebar.markdown("Principais causas de mortes violentas por estado.")
+st.sidebar.markdown("**Principais causas de mortes violentas por estado**")
 
 st.sidebar.subheader("Estado")
 estados_causas = sorted(df_causasviolentas['UF'].unique())
@@ -136,8 +142,11 @@ grouped = df_causasviolentas[df_causasviolentas['UF'] == estado_filtrado_causas]
 df_causasviolentas_new = pd.DataFrame(grouped)
 df_causasviolentas_new.sort_values(by='quantidade', ascending=False, inplace=True)
 df_causasviolentas_new.set_index('descricao', inplace=True)
+
+fig, ax = plt.subplots(figsize=(12, 8))
 plt.title(f'Principais causas de mortes violentas no estado do {estado_filtrado_causas}')
-sns.barplot(x=df_causasviolentas_new['quantidade'][:5], y=df_causasviolentas_new[:5].index)
+ax = sns.barplot(x=df_causasviolentas_new['quantidade'][:5], y=df_causasviolentas_new[:5].index)
+
 st.pyplot()
 
 st.sidebar.markdown("""
